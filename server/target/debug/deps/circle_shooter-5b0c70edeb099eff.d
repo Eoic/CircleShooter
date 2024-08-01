fdbddb73035d143295e92930c2261108b39642c2
Eoic <karolis.strazdas@protonmail.com>
2024-08-01 23:11:53 +0300

    Add .gitignore for the server.

--- server/target/debug/deps/circle_shooter-5b0c70edeb099eff.d
+++
@@ -1,5 +0,0 @@
-/home/karolis/Documents/Projects/circle-shooter/server/target/debug/deps/libcircle_shooter-5b0c70edeb099eff.rmeta: src/main.rs
-
-/home/karolis/Documents/Projects/circle-shooter/server/target/debug/deps/circle_shooter-5b0c70edeb099eff.d: src/main.rs
-
-src/main.rs:
--- server/target/debug/deps/server-6fadb03198841212.d
+++
@@ -1,5 +0,0 @@
-/home/karolis/Documents/Projects/circle-shooter/server/target/debug/deps/libserver-6fadb03198841212.rmeta: src/main.rs
-
-/home/karolis/Documents/Projects/circle-shooter/server/target/debug/deps/server-6fadb03198841212.d: src/main.rs
-
-src/main.rs:
